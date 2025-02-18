955d07b5e50a89d364662e481590a8a80c34c33a
aa <marakuyaspb@gmail.com>
2025-02-18 16:08:10 +0300

    new things about main oage

--- static/js/add_bg.js
+++ static/js/add_bg.js
@@ -5,9 +5,9 @@ function add_bg() {
     setTimeout(function() {
         if (navbarNav.classList.contains("show")) {
             console.log("yes")
-            nav_mob.classList.add("white_dirty_bg");
+            nav_mob.classList.add("orange_so_light_bg");
         } else {
-            nav_mob.classList.remove("white_dirty_bg");
+            nav_mob.classList.remove("orange_so_light_bg");
             console.log("no")
         }
     }, 400); 
